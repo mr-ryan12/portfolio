d634a56d911e4fe7ab6df05a63aac67891914882
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 12:42:02 -0600

    Add project card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import githubLogo from '../../assets/github-logo.png';
 import troubadourLogo from '../../assets/troubadour-logo.png';
-import phishLogo from '../../assets/phish-logo.png'
+import phishLogo from '../../assets/phish-logo.png';
+import artFromTheHeartLogo from '../../assets/art-from-the-heart-logo.png';
 import './Projects.scss';
 
 const Projects = () => {
@@ -26,8 +27,8 @@ const Projects = () => {
         <section className="projects-card">
           <h3 className="project-title">Art From the Heart</h3>
           <section className="logo-container">
-            <a target="_blank" href="https://phish-showcase.vercel.app/"><img src={phishLogo} alt="Phish Logo" className="phish-logo"/></a>
-            <a target="_blank" href="https://github.com/mr-ryan12/phish"><img src={githubLogo} alt="GitHub Logo" className="github-logo"/></a>
+            <a target="_blank" href="https://art-from-the-heart.vercel.app/"><img src={artFromTheHeartLogo} alt="Art from the Heart Logo" className="art-from-the-heart-logo"/></a>
+            <a target="_blank" href="https://github.com/mr-ryan12/art-from-the-heart"><img src={githubLogo} alt="GitHub Logo" className="github-logo"/></a>
           </section>
         </section>
       </section>
