14f49add6f9d7e82a9425b66f48b19d4e7fb4dab
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 12:35:38 -0600

    Add phish project card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import githubLogo from '../../assets/github-logo.png';
 import troubadourLogo from '../../assets/troubadour-logo.png';
+import phishLogo from '../../assets/phish-logo.png'
 import './Projects.scss';
 
 const Projects = () => {
@@ -18,8 +19,15 @@ const Projects = () => {
         <section className="projects-card">
           <h3 className="project-title">Phish</h3>
           <section className="logo-container">
-            <a target="_blank" href="https://troubadour-fe.herokuapp.com/"><img src={troubadourLogo} alt="Troubadour Logo" className="troubadour-logo"/></a>
-            <a target="_blank" href="https://github.com/Troubadour-Project/troubadour-fe"><img src={githubLogo} alt="GitHub Logo" className="github-logo"/></a>
+            <a target="_blank" href="https://phish-showcase.vercel.app/"><img src={phishLogo} alt="Phish Logo" className="phish-logo"/></a>
+            <a target="_blank" href="https://github.com/mr-ryan12/phish"><img src={githubLogo} alt="GitHub Logo" className="github-logo"/></a>
+          </section>
+        </section>
+        <section className="projects-card">
+          <h3 className="project-title">Art From the Heart</h3>
+          <section className="logo-container">
+            <a target="_blank" href="https://phish-showcase.vercel.app/"><img src={phishLogo} alt="Phish Logo" className="phish-logo"/></a>
+            <a target="_blank" href="https://github.com/mr-ryan12/phish"><img src={githubLogo} alt="GitHub Logo" className="github-logo"/></a>
           </section>
         </section>
       </section>
