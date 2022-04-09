6a1c7a2d27e580cb1309f601b6af2f2647b4ca37
Ryan McBride <mr.ryan444@gmail.com>
2022-04-08 22:46:33 -0600

    Add hero image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Landing from './components/Landing/Landing'
 import './App.scss';
 
 const App = () => {
   return (
     <main className="App">
       <Header/>
+      <Landing/>
     </main>
   );
 }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import redRocks from '../../assets/red-rocks.png';
+import './Landing.scss';
+
+const Landing = () => {
+  return (
+    <img src={redRocks} alt="Red Rocks Amphitheatre" className="hero-image"/>
+  )
+}
+
+export default Landing;
