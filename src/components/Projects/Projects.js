7fba14557770fc54e23ce6f6caab1521aa7124cb
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 11:59:40 -0600

    Add github logo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import githubLogo from '../../assets/github-logo.png'
 import './Projects.scss';
 
 const Projects = () => {
@@ -6,8 +7,11 @@ const Projects = () => {
     <section className="projects-container">
       <h2 className="projects-header">Projects</h2>
       <section className="projects-card">
-        <h3>Troubadour</h3>
-
+        <h3 className="project-title">Troubadour</h3>
+        <section className="logo-container">
+          <p>icon</p>
+          <img src={githubLogo} alt="GitHub Logo" className="github-logo"/>
+        </section>
       </section>
     </section>
   )
