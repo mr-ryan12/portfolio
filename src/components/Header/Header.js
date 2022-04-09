8c5aae3b4783496dfbb9c466d1f59956cc442d28
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 14:14:28 -0600

    Add framer motion to nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import profileLogo from '../../assets/profile-icon.png';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <nav>
-      <section className="left-nav-container">
-        <Link to="/"><img src={profileLogo} alt="Profile Picture" className="profile-pic"/></Link>
-        <Link to="/" className="title">Ryan McBride</Link>
-      </section>
-      <section className="link-container">
-        <Link to='/about' className="hover-underline-animation">About</Link>
-        <Link to='/resume' className="hover-underline-animation">Resume</Link>
-        <Link to='/projects' className="hover-underline-animation">Projects</Link>
-        <Link to='/contact' className="hover-underline-animation">Contact</Link>
-        <a target="_blank" href="https://github.com/mr-ryan12" className="hover-underline-animation">GitHub</a>
-        <a target="_blank" href="https://www.linkedin.com/in/j-ryan-mcbride/" className="hover-underline-animation">LinkedIn</a>
-      </section>
-    </nav>
+    <motion.div animate={{ opacity: [0 ,1] }} transition={{ duration: .4 }}>
+      <nav>
+        <section className="left-nav-container">
+          <Link to="/"><img src={profileLogo} alt="Profile Picture" className="profile-pic"/></Link>
+          <Link to="/" className="title">Ryan McBride</Link>
+        </section>
+        <section className="link-container">
+          <Link to='/about' className="hover-underline-animation">About</Link>
+          <Link to='/resume' className="hover-underline-animation">Resume</Link>
+          <Link to='/projects' className="hover-underline-animation">Projects</Link>
+          <Link to='/contact' className="hover-underline-animation">Contact</Link>
+          <a target="_blank" href="https://github.com/mr-ryan12" className="hover-underline-animation">GitHub</a>
+          <a target="_blank" href="https://www.linkedin.com/in/j-ryan-mcbride/" className="hover-underline-animation">LinkedIn</a>
+        </section>
+      </nav>
+    </motion.div>
   )
 }
 
