94f3ae2047ffdc4a06c385585fb1738b6d7aca73
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 14:35:00 -0600

    Add framer motion to resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import resumePDF from '../../assets/resume.pdf';
+import { motion } from 'framer-motion';
 import './Resume.scss';
 
 const Resume = () => {
   return (
-    <iframe src={resumePDF + '#toolbar=0'} className="resume"></iframe>
+    <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1.6 }} className="about-container">
+      <iframe src={resumePDF + '#toolbar=0'} className="resume"></iframe>
+    </motion.div>
   )
 }
 
