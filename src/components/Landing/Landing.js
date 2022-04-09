3ca07f90e66b8eadbe27ea406dde198eaceeda02
Ryan McBride <mr.ryan444@gmail.com>
2022-04-09 14:17:43 -0600

    Add framer motion to hero image

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import redRocks from '../../assets/red-rocks.png';
+import { motion } from 'framer-motion';
 import './Landing.scss';
 
 const Landing = () => {
   return (
-    <section className="hero-image-container">
-      <img src={redRocks} alt="Red Rocks Amphitheatre" className="hero-image"/>
-    </section>
-
+    <motion.div animate={{ opacity: [0 ,1] }} transition={{ duration: 2 }}>
+      <section className="hero-image-container">
+        <img src={redRocks} alt="Red Rocks Amphitheatre" className="hero-image"/>
+      </section>
+    </motion.div>
   )
 }
 
