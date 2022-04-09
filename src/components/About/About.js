606e3d37fe77be5793f8542b9685ddf9c10f1337
Ryan McBride <mr.ryan444@gmail.com>
2022-04-08 23:25:08 -0600

    Add text to container

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+  return (
+    <section className="about-container">
+      <h2 className="about-header">About</h2>
+      <p className="about-text">Hi! Thank you for visiting. I used to be a human services specialist until I discovered enjoyment in building web applications. I'm currently finishing up my frontend engineering program at the Turing School of Software and Design in Denver, CO. Really excited to start my new career as a software developer. Feel free to reach out anytime!</p>
+      {/* <p className="about-text">Lorem ipsum dolar simet some other crap i think its pretty kewl ummmmmkaaaayyyy</p>
+      <p className="about-text">Lorem ipsum dolar simet some other crap i think its pretty kewl ummmmmkaaaayyyy</p>
+      <p className="about-text">Lorem ipsum dolar simet some other crap i think its pretty kewl ummmmmkaaaayyyy</p> */}
+    </section>
+  )
+}
+
+export default About;
